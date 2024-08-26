34c0640fb94f09782dd9620eeeea79eae24811e4
Subhranil <subhra750nil@gmail.com>
2024-08-27 00:00:40 +0530

    About modify

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,9 @@ import React from 'react'
 const About = () => {
 
       return (
-            <div>
-                  About
+            <div className='m-5 h1 fst-italic'>
+                  An online notebook website where you can log in and add your personal notes. You can
+                  see your own notes & update, delete your own notes only.
             </div>
       )
 }
